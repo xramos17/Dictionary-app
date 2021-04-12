99dfaecad9526240f20981eed58cf0cd9edbd9e1
xramos17 <58446715+xramos17@users.noreply.github.com>
2021-04-12 14:39:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+ export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+      <h1 className="dictionary-header"> Dictionary Under Construction</h1>
+      </header>
+    </div>
+  );
+}
+
+

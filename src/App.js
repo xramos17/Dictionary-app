f4eeebffc40b8530788e75bc9aa3b0e72f8f4c62
xramos17 <58446715+xramos17@users.noreply.github.com>
2021-04-12 17:00:20 +0100

    Added Search engine basics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-
 import './App.css';
+import DictionarySearch from "./DictionarySearch.js"; 
 
  export default function App() {
   return (
     <div className="App">
+      <div className="container">
+
       <header className="App-header">
      
-      <h1 className="dictionary-header"> Dictionary Under Construction</h1>
+      <h1 className="dictionary-header"> Dictionary</h1>
+      <h5 className="text-center dictionary-legend ">/What word do you want to look up?/</h5>
+
+     <DictionarySearch />   
+
       </header>
+
+
+
+      <footer className="footer-section m-3">
+       Coded by <a href="https://github.com/xramos17" alt="github address of Alex Ramos" className="text-decoration-none"> Alex Ramos</a> 
+      </footer>
     </div>
+</div>
   );
 }
 
---
+++ src/DictionarySearch.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function DictionarySearch(){
+    let [keyword, setKeyword] = useState("");
+
+ function search(event){
+        event.preventDefault(); 
+     alert(`Searching "${keyword}" definition`);
+
+    }
+    function handleKeywordChange(event){
+    setKeyword(event.target.value);
+   
+    }
+
+   
+
+
+    return(
+        <div className="Dictionary-search text-center">
+          <form onSubmit={search}>
+              <input type="search" className="input-search m-3" onChange={handleKeywordChange} />
+
+
+       </form>
+        </div>
+    );
+};

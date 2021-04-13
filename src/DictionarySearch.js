a96bbfeea3a160845c9936ef2a18a387053807e7
xramos17 <58446715+xramos17@users.noreply.github.com>
2021-04-13 15:16:23 +0100

    Added Dictionary API

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function DictionarySearch(){
-    let [keyword, setKeyword] = useState("");
+
+let [keyword, setKeyword] = useState("");
+
+
+function handleResponse(response){
+console.log(response.data[0]);
+}
 
  function search(event){
-        event.preventDefault(); 
-     alert(`Searching "${keyword}" definition`);
+    event.preventDefault(); 
+     
+     let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`;
+     axios.get(apiUrl).then(handleResponse);
 
     }
+
     function handleKeywordChange(event){
     setKeyword(event.target.value);
    

216581d54cf4da195d4d5608a87d78b977779171
xramos17 <58446715+xramos17@users.noreply.github.com>
2021-04-13 19:29:13 +0100

    API changes

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function Meaning (props){
 return (
-    
     <div>
     <h3> {props.meaning.partOfSpeech}</h3>
     {props.meaning.definitions.map(function(definition, index){

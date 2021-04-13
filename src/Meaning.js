7b5212364f8e15b4020aa37265b17522ad314c39
xramos17 <58446715+xramos17@users.noreply.github.com>
2021-04-13 19:01:36 +0100

    more api changes

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,7 +5,7 @@ return (
     
     <div>
     <h3> {props.meaning.partOfSpeech}</h3>
-    {props.meaning.definition.map(function(definition, index){
+    {props.meaning.definitions.map(function(definition, index){
         return (
             <div key={index}>
 {definition.definition}

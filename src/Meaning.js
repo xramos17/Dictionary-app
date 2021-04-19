7a3cd7b5a0b1f92ac3f745b1bbd466dd20e489c9
xramos17 <58446715+xramos17@users.noreply.github.com>
2021-04-19 21:46:28 +0100

    synonyms add

--- src/Meaning.js
+++ src/Meaning.js
@@ -13,27 +13,16 @@ return (
     <h5 className="text-uppercase "> {props.meaning.partOfSpeech}</h5>
 
     {props.meaning.definitions.map(function(definition, index){
-        return (
-<div key={index}>
-
-             <p><strong>Definition</strong>
-              {definition.definition}
+    return (
+          <div key={index}>
+            <p>
+              <strong>Definition:</strong> {definition.definition}
               <br />
-            <strong>Example</strong>
-             <br />
+              <strong>Example:</strong>
               <em>{definition.example}</em>
-               <br />
-              <strong>Synonyms</strong>
               <Synonyms synonyms={definition.synonyms} />
             </p>
-
-<div>
-
-
-</div>
-
-<br />
-</div>
+          </div>
         );
     })}  
 
